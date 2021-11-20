8fdb4b699293b6dc9c769650dd74c9b3c12ba0df
Shun <shun12baseball@gmail.com>
2021-11-20 09:37:08 +0900

    feat: react + typescriptの構成を動かせるようにした

---
+++ app/ts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,23 @@ const webpack = require('webpack')
 module.exports = {
   mode: "production",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
+    react_app: './app/ts/index.tsx',
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
