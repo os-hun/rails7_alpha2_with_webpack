c1d694baefc3270139a78916e61fb8262ba964c0
Shun <shun12baseball@gmail.com>
2021-11-20 19:34:52 +0900

    fix: scripts, remove mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
-const path    = require("path")
+const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  mode: "production",
   entry: {
-    application: "./app/javascript/application.js",
+    application: './app/javascript/application.js',
     react_app: './app/ts/index.tsx',
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'app/assets/builds'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader'
-      }
-    ]
+        use: 'ts-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
+      maxChunks: 1,
+    }),
+  ],
 }
