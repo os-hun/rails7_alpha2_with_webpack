b2e2dbb13239d68ba0278a1a239ecdb7c9db0207
Shun <shun12baseball@gmail.com>
2021-11-21 17:54:27 +0900

    feat: install react-router-dom.

---
+++ app/ts/src/Routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes as ReactRouterRoutes,
+  Route,
+} from 'react-router-dom'
+import { App } from './App'
+import { LogIn } from './containers/auth/LogIn'
+
+const roots = [
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/auth/log_in',
+    element: <LogIn />,
+  },
+]
+
+export const Routes = () => (
+  <Router>
+    <ReactRouterRoutes>
+      {roots.map((root) => (
+        <Route key={root.path} path={root.path} element={root.element} />
+      ))}
+    </ReactRouterRoutes>
+  </Router>
+)
